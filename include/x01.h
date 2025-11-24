48ba2c45b7e3482959b8000619598d739b7e27af
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-11-24 14:32:34 +0100

    x01.h kész

---
+++ include/x01.h
@@ -0,0 +1,14 @@
+#ifndef X01_H
+#define X01_H
+
+#include "lista.h"
+
+void x01_menu();
+void x01_beallitas(int *x01);
+void leg_beallitas(int *leg);
+void set_beallitas(int *set);
+void x01_jatekos_hozzaadas(int x01_pont, int set, int leg);
+void x01_jatekos_torlese();
+void x01_jatek();
+
+#endif
