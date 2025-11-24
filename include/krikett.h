87e0a70e90251455ba385775ba0868e3ce108658
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-11-24 21:17:49 +0100

    krikett.h kész

---
+++ include/krikett.h
@@ -0,0 +1,10 @@
+#ifndef KRIKETT_H
+#define KRIKETT_H
+
+void krikett_menu();
+void set_beallitas();
+void leg_beallitas();
+void krikett_jatekos_hozzaadas(int set, int leg);
+void krikett_jatekos_torles();
+
+#endif
