6014da05106530928c661c9879c0405abfc28423
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-11-28 14:15:27 +0100

    x01.h javítása

--- include/x01.h
+++ include/x01.h
@@ -1,12 +1,12 @@
 #ifndef X01_H
 #define X01_H
 
-#include "lista.h"
+#include "../include/lista.h"
 
 void x01_menu();
 void x01_beallitas(int *x01);
-void leg_beallitas(int *leg);
-void set_beallitas(int *set);
+void x01_leg_beallitas(int *leg);
+void x01_set_beallitas(int *set);
 void x01_jatekos_hozzaadas(int x01_pont, int set, int leg);
 void x01_jatekos_torlese();
 void x01_jatek();
