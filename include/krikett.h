c31f2c2ea3be9c7922e19738f7689d78c3469512
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-11-28 23:18:31 +0100

    krikett.h módosítás

--- include/krikett.h
+++ include/krikett.h
@@ -10,7 +10,7 @@ void leg_beallitas();
 void krikett_jatekos_hozzaadas(int set, int leg);
 void krikett_jatekos_torles();
 bool ellenoriz_dobasok(Krikett_jatekosok *j, const char *d1, const char *d2, const char *d3);
-bool szamol_dobas(Krikett_jatekosok *j, const char *dobas);
+bool krikett_szamol_dobas(Krikett_jatekosok *j, const char *dobas);
 void szamol_pont(Krikett_jatekosok *j);
 void krikett_jatek();
 
