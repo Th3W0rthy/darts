01a3798ff2ec227aea7d14455a9099ee0bb9658a
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-11-28 23:21:40 +0100

    x01.h javítása

--- include/x01.h
+++ include/x01.h
@@ -1,6 +1,7 @@
 #ifndef X01_H
 #define X01_H
 
+#include "stdbool.h"
 #include "../include/lista.h"
 
 void x01_menu();
@@ -9,6 +10,7 @@ void x01_leg_beallitas(int *leg);
 void x01_set_beallitas(int *set);
 void x01_jatekos_hozzaadas(int x01_pont, int set, int leg);
 void x01_jatekos_torlese();
-void x01_jatek();
+bool x01_szamol_dobas(X01_jatekosok *j, const char *dobas, bool *kilep, bool *sok);
+void x01_jatek(int x01, int set, int leg);
 
 #endif
