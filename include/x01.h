5a22c68adfceb18d9967ac617b78cd60b911c0ce
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-11-29 18:27:30 +0100

    x01.h javítása

--- include/x01.h
+++ include/x01.h
@@ -11,6 +11,7 @@ void x01_set_beallitas(int *set);
 void x01_jatekos_hozzaadas(int x01_pont, int set, int leg);
 void x01_jatekos_torlese();
 bool x01_szamol_dobas(X01_jatekosok *j, const char *dobas, bool *kilep, bool *sok);
+void kiir_kiszallo(const int x01);
 void x01_jatek(int x01, int set, int leg);
 
 #endif
