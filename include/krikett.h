875674fc291e755bbddf8c8955e422f7e2a70647
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-11-30 19:05:04 +0100

    krikett.h javítása

--- include/krikett.h
+++ include/krikett.h
@@ -11,7 +11,7 @@ void krikett_jatekos_torles();
 bool krikett_szamol_dobas(Krikett_jatekosok *j, const char *dobas, bool *kilep);
 bool ellenoriz_dobasok(Krikett_jatekosok *j, const char *d1, const char *d2, const char *d3, bool *kilep);
 void szamol_pont(Krikett_jatekosok *j);
-void mentes(Krikett_jatekosok *j);
+void krikett_mentes();
 void krikett_jatek();
 void krikett_felszabadit();
 void krikett_menu();
