224a0ee8cba9fd1a76d5ef4d7c7c6f57e244a985
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-12-02 20:38:00 +0100

    krikett.h javítása

--- include/krikett.h
+++ include/krikett.h
@@ -4,16 +4,16 @@
 #include "stdbool.h"
 #include "../include/lista.h"
 
-void set_beallitas();
-void leg_beallitas();
-void krikett_jatekos_hozzaadas(int set, int leg);
-void krikett_jatekos_torles();
+void krikett_set_beallitas(Krikett_jatekosok *eleje, int *set);
+void krikett_leg_beallitas(Krikett_jatekosok *eleje, int *leg);
+void krikett_jatekos_hozzaadas(Krikett_jatekosok **eleje, int set, int leg);
+void krikett_jatekos_torles(Krikett_jatekosok **eleje);
 bool krikett_szamol_dobas(Krikett_jatekosok *j, const char *dobas, bool *kilep);
 bool ellenoriz_dobasok(Krikett_jatekosok *j, const char *d1, const char *d2, const char *d3, bool *kilep);
 void szamol_pont(Krikett_jatekosok *j);
-void krikett_mentes();
-void krikett_jatek();
-void krikett_felszabadit();
+void krikett_mentes(Krikett_jatekosok *eleje);
+void krikett_jatek(Krikett_jatekosok *eleje);
+void krikett_felszabadit(Krikett_jatekosok **eleje);
 void krikett_menu();
 
 #endif
