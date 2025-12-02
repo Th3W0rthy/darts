b7ea959c6e010442f15646ffa074805f2038623a
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-12-02 20:24:33 +0100

    x01.h javítása

--- include/x01.h
+++ include/x01.h
@@ -4,16 +4,16 @@
 #include "stdbool.h"
 #include "../include/lista.h"
 
-void x01_beallitas(int *x01);
-void x01_leg_beallitas(int *leg);
-void x01_set_beallitas(int *set);
-void x01_jatekos_hozzaadas(int x01_pont, int set, int leg);
-void x01_jatekos_torlese();
+void x01_set_beallitas(X01_jatekosok *X01_eleje, int *set);
+void x01_leg_beallitas(X01_jatekosok *X01_eleje, int *leg);
+void x01_beallitas(X01_jatekosok *X01_eleje, int *x01);
+void x01_jatekos_hozzaadas(X01_jatekosok **X01_eleje, int x01, int set, int leg);
+void x01_jatekos_torlese(X01_jatekosok **X01_eleje);
 bool x01_szamol_dobas(X01_jatekosok *j, const char *dobas, bool *kilep, bool *sok);
-void kiir_kiszallo(const int x01, X01_jatekosok *j);
-void x01_mentes();
-void x01_jatek(int x01, int set, int leg);
-void x01_felszabadit();
+void kiir_kiszallo(const int x01);
+void x01_mentes(X01_jatekosok *X01_eleje);
+void x01_jatek(X01_jatekosok *X01_eleje, int x01, int set, int leg);
+void x01_felszabadit(X01_jatekosok **X01_eleje);
 void x01_menu();
 
 #endif
